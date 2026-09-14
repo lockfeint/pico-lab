1e802d83c4ad892c99778d1597c3a799d670b68d
LockFeint <280072915+lockfeint@users.noreply.github.com>
2026-09-14 22:23:56 +0800

    Feature: Walking and Running Actions

--- prototype-1/src/player.lua
+++ prototype-1/src/player.lua
@@ -2,24 +2,60 @@ player = {
   spr = 1,
   x = 0,
   y = 0,
+  speed = 1,
+  facing = 'right',
+  action = 'idle',
   init = function(self) 
     self.spr = 1
     self.x = 0
     self.y = 0
   end,
   control = function(self)
+    self.action = 'idle'
+
     if btn(0) then
-      self.x -= 1
+      self.x -= self.speed
+      self.facing = 'left'
+      self.action = 'walk'
     elseif btn(1) then
-      self.x += 1
+      self.x += self.speed
+      self.facing = 'right'
+      self.action = 'walk'
     end
     if btn(2) then
-      self.y -= 1
+      self.y -= self.speed
+      self.action = 'walk'
     elseif btn(3) then
-      self.y += 1
+      self.y += self.speed
+      self.action = 'walk'
+    end
+
+    if btn(5) then
+      self.speed = 2
+      self.action = 'run'
+    else
+      self.speed = 1
+    end
+
+  end,
+  animate = function(self)
+    self.spr += 0.25
+    if self.action == 'walk' then
+      if self.spr >= 4 then
+        self.spr = 2
+      end
+    elseif self.action == 'run' then
+      if self.spr >= 10 then
+        self.spr = 5
+      end
+    else
+      self.spr = 1
     end
   end,
-  animate = function(self) 
-    spr(self.spr, self.x, self.y)
+  draw = function(self)
+    palt(0, false)
+    palt(3, true)
+    local flip_x = self.facing == 'left'
+    spr(self.spr, self.x, self.y, 1, 1, flip_x)
   end
 }
