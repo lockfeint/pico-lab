c74077a5982cae86cbcec5f5d4daf6f64317d0d7
LockFeint <280072915+lockfeint@users.noreply.github.com>
2026-09-20 07:29:16 +0800

    Feature: Normal Attack

--- prototype-1/src/player.lua
+++ prototype-1/src/player.lua
@@ -4,11 +4,15 @@ player = {
   y = 0,
   facing = 'right',
   action = 'idle',
+  animation_lock = false,
+  action_combo = {},
 
   init = function(self) 
     self.spr = 1
     self.x = 0
     self.y = 0
+    animation_lock = false
+    action_combo = {}
   end,
 
   move = function(self, action, direction)
@@ -33,13 +37,31 @@ player = {
     end
   end,
 
+  attack = function(self)
+    if contains(self.action_combo, 'p2') then
+      add(self.action_combo, 'p3')
+    elseif contains(self.action_combo, 'p1') then
+      add(self.action_combo, 'p2')
+    else
+      self.action = 'attack'
+      add(self.action_combo, 'p1')
+      self.animation_lock = true
+    end
+  end,
+
   control = function(self)
     local move_action = 'walk'
 
-    self.action = 'idle'
-
-    if btn(5) then
+    if btnp(4) then
+      self:attack()
+    elseif btn(5) then
       move_action = 'run'
+    end
+
+    if self.animation_lock then
+      return
+    else
+      self.action = 'idle'
     end
 
     if btn(0) then
@@ -55,14 +77,31 @@ player = {
   end,
 
   animate = function(self)
-    self.spr += 0.25
     if self.action == 'walk' then
+      self.spr += 0.25
       if self.spr >= 4 then
         self.spr = 2
       end
     elseif self.action == 'run' then
+      self.spr += 0.25
       if self.spr >= 10 then
         self.spr = 5
+      end
+    elseif self.action == 'attack' then
+      local end_spr = 13
+      if contains(self.action_combo, 'p3') then
+        end_spr = 24
+      elseif contains(self.action_combo, 'p2') then
+        end_spr = 15
+      end
+      if self.spr < 10 then
+        self.spr = 10
+      end
+      self.spr += 0.40
+      if self.spr >= end_spr then
+        self.spr = 1
+        self.action_combo = {}
+        self.animation_lock = false
       end
     else
       self.spr = 1
